3b81442da805c87af9907af093e8f01f13882117
Markus Reichenbach <reichi62@web.de>
2022-01-30 09:08:58 +0100

    TripDB V1.1.3 Working with Django Environment

--- manage.py
+++ manage.py
@@ -6,9 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'DevConfig')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tripdb_prj.settings')
     try:
-        from django.core.management import execute_from_command_line
+        #from django.core.management import execute_from_command_line
+        from configurations.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
